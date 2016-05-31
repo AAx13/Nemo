446354a662bdf97b4b1a3a76f5203bfdbe990723
DamianAli <damian.ali33@gmail.com>
2016-05-31 14:36:53 -0700

    Finished installation file

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ clear
 
     read -p "Please Press 1 to Install or 2 to Uninstall --> : " INPUT
     if [[ "$INPUT" == 1 ]]; then
-	          sudo cp ./shellMen /bin/
-	          sudo chown root:root /bin/shellMen
-	          sudo chmod +x /bin/shellMen
+            brew install dialog
+	          sudo cp ./nemo /bin/
+	          sudo chown root:root /bin/nemo
+	          sudo chmod +x /bin/nemo
     elif [[ "$INPUT" == 2 ]]; then
-          	sudo rm /bin/shellMen
+          	sudo rm /bin/nemo
     elif [[ "$INPUT" != 1 || "$INPUT" != 2 ]] ; then
            echo "Please type 1 or 2."
            sleep 2
